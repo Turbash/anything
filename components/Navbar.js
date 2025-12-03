03045c5e507ce43ca7a6c81b07aad762cf2e3c77
Turbash <negirawatdeepi@gmail.com>
2025-12-03 11:58:38 +0530

    Added theme and responsiveness for mobile still figuring out things in next js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,82 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { ModeToggle } from "./ThemeBtn";
 
 const Navbar = () => {
   return (
-    <nav className="p-4">
+    <nav className="p-4 bg-background/50 sticky top-0 backdrop-blur border-b">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">SiegeBlog</div>
-        <div className="md:flex space-x-4 hidden">
+        <Link href="/">
+          <div className="text-lg font-bold">SiegeBlog</div>
+        </Link>
+        <div className="md:flex space-x-4 hidden items-center">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
+          <div className="flex items-center">
+            <Button className="mx-1" variant="outline">
+              Login
+            </Button>
+            <Button className="mx-1" variant="outline">
+              Sign Up
+            </Button>
+            <ModeToggle />
+          </div>
         </div>
         <div className="md:hidden">
-          <button className="focus:outline-none">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h26M4 12h16m-7"
-              ></path>
-            </svg>
-          </button>
+          <span className="mx-2">
+            <ModeToggle />
+          </span>
+          <Sheet>
+            <SheetTrigger>
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="font-bold my-4">SiegeBlog</SheetTitle>
+              </SheetHeader>
+              <SheetDescription />
+              Something will go here i guess
+              <SheetDescription />
+              <div className="gap-6 flex flex-col items-center">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/contact">Contact</Link>
+                <div>
+                  <Button className="mx-1 text-xs" variant="outline">
+                    Login
+                  </Button>
+                  <Button className="mx-1 text-xs" variant="outline">
+                    Sign Up
+                  </Button>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </nav>
