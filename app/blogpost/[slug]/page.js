8b25f892c4fff7688370c29ee67477820bf90cd8
Turbash <negirawatdeepi@gmail.com>
2025-12-03 17:44:47 +0530

    now the blog part is fixed it was await required in params now i will move to format part using unified

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -1,7 +1,14 @@
 import fs from "fs";
 import { notFound } from "next/navigation";
+import matter from "gray-matter";
+import rehypeDocument from "rehype-document";
+import rehypeFormat from "rehype-format";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 
-export default function Page({ params }) {
+export default async function Page({ params }) {
   //   const blog = {
   //     title: "Typescript tutorial in hindi",
   //     author: "John Doe",
@@ -11,27 +18,37 @@ export default function Page({ params }) {
   //       "<p>This is the content of the blog post. It can include <strong>HTML</strong> tags and other elements.</p>",
   //   };
 
-  const filePath = `content/${params.slug}.md`;
+  const { slug } = await params;
+  const filePath = `content/${slug}.md`;
 
   if (!fs.existsSync(filePath)) {
     notFound();
   }
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { content, data } = matter(fileContent);
-  console.log(blog, content);
+  //   console.log(data, content);
+
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument, { title: "👋🌍" })
+    .use(rehypeFormat)
+    .use(rehypeStringify);
+
+  const htmlContent = (await processor.process(content)).toString();
 
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">{blog.title}</h1>
+      <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
       <p className="text-base mb-2 border-l-4 border-gray-500 pl-4 italic">
-        &quot;{blog.description}&quot;
+        &quot;{data.description}&quot;
       </p>
       <div className="flex gap-2">
-        <p className="text-sm text-gray-500 mb-4 italic">By {blog.author}</p>
-        <p className="text-sm text-gray-500 mb-4">{blog.date}</p>
+        <p className="text-sm text-gray-500 mb-4 italic">By {data.author}</p>
+        <p className="text-sm text-gray-500 mb-4">{data.date}</p>
       </div>
       <div
-        dangerouslySetInnerHTML={{ __html: blog.content }}
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
         className="prose dark:prose-invert"
       ></div>
     </div>
